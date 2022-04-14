15f4a4fa8d1be3000f2e325b6bad3ecb179d181b
Petia Latinova <petia@hack.bg>
2022-04-14 17:14:40 +0300

    ops: deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,30 +1,81 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
 import { ethers } from "hardhat";
+import { ContractFactory } from "ethers";
 
-async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+// The deploy script works for BeefyFinance on Polygon Mainnet.
+// For other harvesters and/or blockchains change the contract name, keeper registry, vault registry address and gas limits.
+const keeperRegistry = "0x7b3EC232b08BD7b4b3305BE0C044D907B2DF960B";
+const vaultRegistry = "0x820cE73c7F15C2b828aBE79670D7e61731AB93Be";
+const config = {
+  harvester: {
+    contractName: "BeefyHarvester",
+    args: {
+      vaultRegistry,
+      keeperRegistry,
+      performUpkeepGasLimit: 2500000,
+      performUpkeepGasLimitBuffer: 100000,
+      vaultHarvestFunctionGasOverhead: 600000,
+      keeperRegistryGasOverhead: 80000,
+    },
+  },
+};
 
-  // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+const deploy = async () => {
+  await deployHarvester();
+  console.log("Deployment is successful");
+};
 
-  await greeter.deployed();
+const deployHarvester = async () => {
+  const BeefyHarvesterFactory = await ethers.getContractFactory(
+    config.harvester.contractName
+  );
 
-  console.log("Greeter deployed to:", greeter.address);
-}
+  const {
+    vaultRegistry,
+    keeperRegistry,
+    performUpkeepGasLimit,
+    performUpkeepGasLimitBuffer,
+    vaultHarvestFunctionGasOverhead,
+    keeperRegistryGasOverhead,
+  } = config.harvester.args;
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+  const harvesterConstructorArguments: any[] = [
+    vaultRegistry,
+    keeperRegistry,
+    performUpkeepGasLimit,
+    performUpkeepGasLimitBuffer,
+    vaultHarvestFunctionGasOverhead,
+    keeperRegistryGasOverhead,
+  ];
+
+  await deployContract(
+    config.harvester.contractName,
+    BeefyHarvesterFactory,
+    harvesterConstructorArguments
+  );
+};
+
+const deployContract = async (
+  name: string,
+  contractFactory: ContractFactory,
+  constructorArgs: any[]
+): Promise<{ contract: string; impl: string }> => {
+  console.log("Deploying:", name);
+  const ret = {
+    contract: "",
+    impl: "",
+  };
+  const deployTx = await contractFactory.deploy(...constructorArgs);
+  await deployTx.deployed();
+  console.log(`${name}: ${deployTx.address}`);
+  const contract = await ethers.getContractAt(name, deployTx.address);
+  ret.contract = contract.address;
+
+  return ret;
+};
+
+deploy()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
