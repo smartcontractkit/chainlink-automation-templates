23e600375ddf2d89583e7ba99e41ed44ee1adaaf
ivaylonikolov7 <ivnikolov721@gmail.com>
2022-08-12 05:41:45 +0300

    feat: add collections page and mint section

--- batch-nft-reveal/hardhat.config.ts
+++ batch-nft-reveal/hardhat.config.ts
@@ -60,6 +60,9 @@ const config: HardhatUserConfig = {
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY,
   },
+  paths: {
+    artifacts: "./app/artifacts",
+  },
 };
 
 export default config;
