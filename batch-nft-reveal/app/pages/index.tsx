d8a3d042354b485c8c80ae2ab96790835f625f90
ivaylonikolov7 <ivnikolov721@gmail.com>
2022-08-26 13:58:03 +0300

    feat: add deploy function

--- batch-nft-reveal/app/pages/index.tsx
+++ batch-nft-reveal/app/pages/index.tsx
@@ -1,12 +1,39 @@
-import { Heading, Text } from '@chakra-ui/react'
+import { Button, Heading, Text } from '@chakra-ui/react'
+import { useEthers } from '@usedapp/core'
 import { Layout } from '../components/layout'
+import { deployNFTCollection } from '../lib/deploy'
+import { ethers } from '@usedapp/core/node_modules/ethers'
+import NFTCollectionParams from '../lib/types/NFTCollectionParams'
 
 function HomeIndex(): JSX.Element {
+  const { chainId, library } = useEthers()
+
   return (
     <>
       <Heading as="h1" mb="4">
         Welcome
       </Heading>
+      <Button
+        onClick={() => {
+          const nftCollectionParams: NFTCollectionParams = {
+            nftName: 'Your Collection Name',
+            nftSymbol: 'ABC',
+            nftMaxSupply: 1000,
+            nftMintCost: ethers.utils.parseEther('0.1'),
+            nftRevealBatchSize: 10,
+            nftRevealInterval: 3600,
+            vrfSubscriptionId: 0,
+          }
+
+          deployNFTCollection(
+            nftCollectionParams,
+            library?.getSigner(),
+            chainId
+          )
+        }}
+      >
+        Deploy NFT Collection
+      </Button>
       <Text fontSize="xl">...</Text>
     </>
   )
---
+++ batch-nft-reveal/app/pages/load.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Heading } from '@chakra-ui/react'
+import { Load } from '../components/load'
+import { Section } from '../components/layout'
+
+function LoadPage(): JSX.Element {
+  return (
+    <>
+      <Heading as="h1" mb="8">
+        Load NFT Collection
+      </Heading>
+      <Section>
+        <Load />
+      </Section>
+    </>
+  )
+}
+
+export default LoadPage
