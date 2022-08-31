07b2c6a8daf1b60af4c21a95e74a06a18f06d8a7
ivaylonikolov7 <ivnikolov721@gmail.com>
2022-08-31 14:54:59 +0300

    feat: use checkUpkeep for instant switch to pending screen

--- batch-nft-reveal/app/components/collection/reveal-info/Revealinfo.tsx
+++ batch-nft-reveal/app/components/collection/reveal-info/Revealinfo.tsx
@@ -1,4 +1,5 @@
 import { Container, Heading } from '@chakra-ui/react'
+import { ethers } from 'ethers'
 import { useContractCall } from '../../../hooks/useContractCall'
 import { useNextRevealAmount } from '../../../hooks/useNextRevealAmount'
 import { useNextRevealTime } from '../../../hooks/useNextRevealTime'
@@ -10,12 +11,16 @@ export const RevealInfo = (props: AddressProp): JSX.Element => {
 
   const nextRevealBatchSize = useNextRevealAmount(contractAddress)
   const nextRevealTime = useNextRevealTime(contractAddress)
-  const isPending = useContractCall('pendingReveal', [], contractAddress)
+  const checkUpkeep = useContractCall(
+    'checkUpkeep',
+    [ethers.constants.HashZero],
+    contractAddress
+  )
 
   return (
     <Container maxWidth="100%" textAlign="center">
-      {isPending ? (
-        <Heading>Pending</Heading>
+      {checkUpkeep ? (
+        <Heading>Pending batch reveal</Heading>
       ) : (
         <>
           <Heading>Next reveal after</Heading>
