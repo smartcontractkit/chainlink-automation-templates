ac926d8126561ec3fc20faf7c0a2d0b393e8df04
ivaylonikolov7 <ivnikolov721@gmail.com>
2022-08-31 14:57:39 +0300

    feat: if interval has passed ask for minting a nft

--- batch-nft-reveal/app/components/collection/reveal-info/NextReveal.tsx
+++ batch-nft-reveal/app/components/collection/reveal-info/NextReveal.tsx
@@ -7,20 +7,26 @@ export const NextReveal = (props: RevealProp): JSX.Element => {
     props.nextRevealInterval &&
     moment.unix(props.nextRevealInterval.toNumber()).fromNow()
 
+  const hasPassed =
+    props.nextRevealInterval &&
+    moment.unix(props.nextRevealInterval.toNumber()).diff(Date.now()) < 0
+      ? true
+      : false
+
   const nextRevealBatchSize =
     props.nextRevealBatchSize && props.nextRevealBatchSize.toString()
-  console.log(nextRevealBatchSize + ' NFTS' || '... ')
-  return (
-    <>
-      <Box marginTop="5">
-        <Text fontWeight="bold" as="span" marginRight="3">
-          {nextRevealBatchSize ? `${nextRevealBatchSize} NFTS` : '... '}
-        </Text>
-        <Text as="span">or</Text>
-        <Text fontWeight="bold" as="span" marginLeft="3">
-          {nextRevealTime || '...'}
-        </Text>
-      </Box>
-    </>
+
+  return hasPassed == true ? (
+    <Text fontWeight={'bold'}>1+ NFTs are minted</Text>
+  ) : (
+    <Box marginTop="5">
+      <Text fontWeight="bold" as="span" marginRight="3">
+        {nextRevealBatchSize ? `${nextRevealBatchSize} NFTS` : '... '}
+      </Text>
+      <Text as="span">or</Text>
+      <Text fontWeight="bold" as="span" marginLeft="3">
+        {nextRevealTime || '...'}
+      </Text>
+    </Box>
   )
 }
