780f6635b711fc7cbdfea16de8a6427efe30a0a1
imollov <ivomollov@gmail.com>
2022-09-12 14:36:43 +0300

    refactor: consolidate layout containers

--- batch-nft-reveal/app/components/layout/Layout.tsx
+++ batch-nft-reveal/app/components/layout/Layout.tsx
@@ -81,108 +81,100 @@ export const Layout = ({ children, customMeta }: LayoutProps): JSX.Element => {
   return (
     <>
       <Head customMeta={customMeta} />
-      <header>
-        <Container maxWidth="container.xl">
-          <SimpleGrid
-            columns={[1, 1, 1, 2]}
-            alignItems="center"
-            justifyContent="space-between"
-            py="8"
-          >
-            <Flex py={[4, null, null, 0]}>
-              <NextLink href="/" passHref>
-                <Link px="4" py="1">
-                  Home
-                </Link>
-              </NextLink>
-              <NextLink href="/create" passHref>
-                <Link px="4" py="1">
-                  Create
-                </Link>
-              </NextLink>
-              <NextLink href="/open" passHref>
-                <Link px="4" py="1">
-                  Open
-                </Link>
-              </NextLink>
-            </Flex>
-            {account ? (
-              <Flex
-                order={[-1, null, null, 2]}
-                alignItems={'center'}
-                justifyContent={['flex-start', null, null, 'flex-end']}
-              >
-                <Balance />
-                <Image ml="4" src={blockieImageSrc} alt="blockie" />
-                <Menu placement="bottom-end">
-                  <MenuButton as={Button} ml="4">
-                    {truncateHash(account)}
-                  </MenuButton>
-                  <MenuList>
-                    <MenuItem
-                      onClick={() => {
-                        deactivate()
-                      }}
-                    >
-                      Disconnect
-                    </MenuItem>
-                  </MenuList>
-                </Menu>
-              </Flex>
-            ) : (
-              <ConnectWallet />
-            )}
-          </SimpleGrid>
-        </Container>
-      </header>
-      <main>
-        <Container maxWidth="container.xl">
-          {error && <Error message={getErrorMessage(error)} />}
-          {children}
-          {notifications.map((notification) => {
-            if (notification.type === 'walletConnected') {
-              return null
-            }
-            return (
-              <Alert
-                key={notification.id}
-                status="success"
-                position="fixed"
-                bottom="8"
-                right="8"
-                width="400px"
-              >
-                <AlertIcon />
-                <Box>
-                  <AlertTitle>
-                    {notification.transactionName}{' '}
-                    {TRANSACTION_TYPE_TITLES[notification.type]}
-                  </AlertTitle>
-                  <AlertDescription overflow="hidden">
-                    Transaction Hash:{' '}
-                    {truncateHash(notification.transaction.hash, 61)}
-                  </AlertDescription>
-                </Box>
-              </Alert>
-            )
-          })}
-        </Container>
-      </main>
-      <footer>
-        <Container mt="8" py="8" maxWidth="container.xl">
-          <HStack>
-            <Link
-              href="https://github.com/hackbg/chainlink-keepers-templates"
-              isExternal
+      <Container as="header" maxWidth="container.xl">
+        <SimpleGrid
+          columns={[1, 1, 1, 2]}
+          alignItems="center"
+          justifyContent="space-between"
+          py="8"
+        >
+          <Flex py={[4, null, null, 0]}>
+            <NextLink href="/" passHref>
+              <Link px="4" py="1">
+                Home
+              </Link>
+            </NextLink>
+            <NextLink href="/create" passHref>
+              <Link px="4" py="1">
+                Create
+              </Link>
+            </NextLink>
+            <NextLink href="/open" passHref>
+              <Link px="4" py="1">
+                Open
+              </Link>
+            </NextLink>
+          </Flex>
+          {account ? (
+            <Flex
+              order={[-1, null, null, 2]}
+              alignItems={'center'}
+              justifyContent={['flex-start', null, null, 'flex-end']}
             >
-              <HStack>
-                <Image src="images/github.svg" width="20px" />
-                <Text>GitHub</Text>
-              </HStack>
-            </Link>
+              <Balance />
+              <Image ml="4" src={blockieImageSrc} alt="blockie" />
+              <Menu placement="bottom-end">
+                <MenuButton as={Button} ml="4">
+                  {truncateHash(account)}
+                </MenuButton>
+                <MenuList>
+                  <MenuItem
+                    onClick={() => {
+                      deactivate()
+                    }}
+                  >
+                    Disconnect
+                  </MenuItem>
+                </MenuList>
+              </Menu>
+            </Flex>
+          ) : (
+            <ConnectWallet />
+          )}
+        </SimpleGrid>
+      </Container>
+      <Container as="main" maxWidth="container.xl">
+        {error && <Error message={getErrorMessage(error)} />}
+        {children}
+        {notifications.map((notification) => {
+          if (notification.type === 'walletConnected') {
+            return null
+          }
+          return (
+            <Alert
+              key={notification.id}
+              status="success"
+              position="fixed"
+              bottom="8"
+              right="8"
+              width="400px"
+            >
+              <AlertIcon />
+              <Box>
+                <AlertTitle>
+                  {notification.transactionName}{' '}
+                  {TRANSACTION_TYPE_TITLES[notification.type]}
+                </AlertTitle>
+                <AlertDescription overflow="hidden">
+                  Transaction Hash:{' '}
+                  {truncateHash(notification.transaction.hash, 61)}
+                </AlertDescription>
+              </Box>
+            </Alert>
+          )
+        })}
+      </Container>
+      <HStack as="footer" mt="8" py="8" px="4" maxWidth="container.xl">
+        <Link
+          href="https://github.com/hackbg/chainlink-keepers-templates"
+          isExternal
+        >
+          <HStack>
+            <Image src="images/github.svg" width="20px" />
+            <Text>GitHub</Text>
           </HStack>
-        </Container>
-      </footer>
+        </Link>
+      </HStack>
     </>
   )
 }
