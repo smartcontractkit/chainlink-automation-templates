28c2ccd6f6828396711255dc584a13c690091d88
ivaylonikolov7 <ivnikolov721@gmail.com>
2023-03-07 15:43:25 +0200

    chore(batch-nft-reveal): add sepolia support to hardhat project

--- batch-nft-reveal/hardhat.config.ts
+++ batch-nft-reveal/hardhat.config.ts
@@ -41,6 +41,11 @@ const config: HardhatUserConfig = {
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
+    sepolia: {
+      url: process.env.SEPOLIA_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
   },
   namedAccounts: {
     deployer: {
--- batch-nft-reveal/helper-hardhat-config.ts
+++ batch-nft-reveal/helper-hardhat-config.ts
@@ -24,6 +24,14 @@ export const networkConfig: Record<
     callbackGasLimit: "500000",
     subscriptionId: "0",
   },
+  "11155111": {
+    name: "sepolia",
+    vrfCoordinatorV2: "0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625",
+    gasLane:
+      "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c",
+    callbackGasLimit: "500000",
+    subscriptionId: "0",
+  },
 };
 
 export const developmentChains = ["31337"];
