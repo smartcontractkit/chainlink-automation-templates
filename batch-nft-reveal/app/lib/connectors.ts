f0bd1efc1322b48cd80872ab6250cd8d7db5a675
ivaylonikolov7 <ivnikolov721@gmail.com>
2023-03-07 15:40:48 +0200

    feat(batch-nft-reveal): add goerli support to frontend

--- batch-nft-reveal/app/lib/connectors.ts
+++ batch-nft-reveal/app/lib/connectors.ts
@@ -4,8 +4,9 @@ const INFURA_KEY = process.env.NEXT_PUBLIC_INFURA_KEY
 
 const RPC_URLS: { [chainId: number]: string } = {
   5: `https://goerli.infura.io/v3/${INFURA_KEY}`,
+  11155111: `https://sepolia.infura.io/v3/${INFURA_KEY}`,
 }
 export const walletconnect = new WalletConnectConnector({
-  rpc: { 5: RPC_URLS[5] },
+  rpc: { 5: RPC_URLS[5], 11155111: RPC_URLS[11155111] },
   qrcode: true,
 })
